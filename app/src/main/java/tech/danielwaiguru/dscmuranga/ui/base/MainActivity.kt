46334b9b9ae63277ae20cf46990ecc35bb7f31e5
Daniel Waiguru <danielirungu91@gmail.com>
2021-03-20 16:28:45 +0300

    Get current loggedin user session and bavigate accordingly

--- app/src/main/java/tech/danielwaiguru/dscmuranga/ui/base/MainActivity.kt
+++ app/src/main/java/tech/danielwaiguru/dscmuranga/ui/base/MainActivity.kt
@@ -11,13 +11,29 @@ import tech.danielwaiguru.dscmuranga.R
 import tech.danielwaiguru.dscmuranga.ui.auth.login.SignInViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val signInViewModel: SignInViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_DscMuranga_NoActionBar)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getSession()
     }
     private fun setupToolbar() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+    }
+    private fun getSession() {
+        val navHostFragment = supportFragmentManager
+                .findFragmentById(R.id.nav_host_container) as NavHostFragment
+        val inflater = navHostFragment.navController.navInflater
+        val graph = inflater.inflate(R.navigation.nav_graph)
+        if (signInViewModel.getCurrentUser() != null) {
+            graph.startDestination = R.id.homeFragment
+        } else {
+            graph.startDestination = R.id.signInFragment
+        }
+        val navController = navHostFragment.navController
+        navController.setGraph(graph, intent.extras)
+
     }
 }
